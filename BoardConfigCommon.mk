0d325e6fc889045218bf441b2ca4e19a93c681a7
aways <aways@outlook.com>
2013-06-16 17:12:28 +0200

    Revert "Semaphorisation: Change kernel source path"
    This reverts commit 1f507c543a6928f524df7b1922925d48761270af.
    
    Conflicts:
    	BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,7 +40,9 @@ BOARD_MOBILEDATA_INTERFACE_NAME = "pdp0"
 
  # Inline kernel building
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6/bin/arm-eabi-
-TARGET_KERNEL_SOURCE := kernel/samsung/samsung-kernel-aries
+
+# Kernel Source
+TARGET_KERNEL_SOURCE := kernel/samsung/aries
 TARGET_KERNEL_CUSTOM_RAMDISK := true
 TARGET_KERNEL_SUPPORTS_HUGEMEM := true
 
